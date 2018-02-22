14de8667e365750dc95460589a3e9cd442a3b423
Nathan Bailey <dev@dnbailey.org>
2018-02-22 08:20:26 -0500

    Add items

---
+++ src/items.py
@@ -0,0 +1,13 @@
+from adventurelib import *
+from rooms import *
+
+#: Configure bags
+Room.items = Bag()
+inventory = Bag()
+
+#: Items
+broom = Item('a broken broom that is a fragment of a destroyed node', 'broom')
+node2.items = Bag({broom})
+
+shell = Item('an oddly shaped shell is a fragment of a destroyed node', 'shell')
+node3.items = Bag({shell})
